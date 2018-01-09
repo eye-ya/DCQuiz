b8bf5e8c55e355f69b6aa3b57db60a066d23f252
Ashley-Kristine <pocketfulofash@gmail.com>
2018-01-08 22:28:05 -0500

    third commit Its finally all coming together. It functions how i want it to for the most part I just need to tell the user which questions they got wrong. Also I'm going to try to make it more aesthetically pleasing.

--- app/src/main/java/com/example/android/dcquiz/MainActivity.java
+++ app/src/main/java/com/example/android/dcquiz/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import java.util.*;
 
@@ -18,52 +19,95 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    EditText textAns1 = (EditText) findViewById(R.id.text);
-    String value1 = textAns1.getText().toString();
-    boolean correctText1 = Boolean.parseBoolean("zambesi");
+    HashMap<String,Boolean> answers = new HashMap<>();
 
+    public boolean question3(boolean ansCorrect2, boolean ansCorrect3){
 
-    EditText textAns2 = (EditText) findViewById(R.id.text2);
-    String value2 = textAns1.getText().toString();
-    boolean correctText2 = Boolean.parseBoolean("cyborg");
+        if (ansCorrect2&&ansCorrect3){
+            return true;
+        }else {
+            return false;
+        }
+    }
 
+    public boolean question6(boolean ansCorrect5, boolean ansCorrect6, boolean ansCorrect7){
 
-    RadioButton ans1 = (RadioButton) findViewById(R.id.q1c1);
-    boolean ans1Correct = ans1.isChecked();
+        if (ansCorrect5&&ansCorrect6&&ansCorrect7){
+            return true;
+        }else {
+            return false;
+        }
 
-    CheckBox ans2 = (CheckBox) findViewById(R.id.q2c1);
-    boolean ans2Correct = ans2.isChecked();
-    CheckBox ans3 = (CheckBox) findViewById(R.id.q2c2);
-    boolean ans3Correct = ans3.isChecked();
-
-    RadioButton ans4 = (RadioButton) findViewById(R.id.q4c1);
-    boolean ans4Correct = ans4.isChecked();
-
-    CheckBox ans5 = (CheckBox) findViewById(R.id.q6c1);
-    boolean ans5Correct = ans5.isChecked();
-    CheckBox ans6 = (CheckBox) findViewById(R.id.q6c2);
-    boolean ans6Correct = ans6.isChecked();
-    CheckBox ans7 = (CheckBox) findViewById(R.id.q6c3);
-    boolean ans7Correct = ans7.isChecked();
+    }
 
     public void submit (View view ){
-        boolean[]trues = {correctText1,correctText2, ans1Correct,ans2Correct,ans3Correct,ans4Correct,ans5Correct,ans6Correct,ans7Correct};
 
-        correct(trues);
-    }
+        EditText textAns1 = (EditText) findViewById(R.id.text);
+        String value1 = textAns1.getText().toString();
+        boolean correctText1 = Boolean.parseBoolean("zambesi");
 
 
-    public  boolean correct( boolean [] trues) {
+        EditText textAns2 = (EditText) findViewById(R.id.text2);
+        String value2 = textAns1.getText().toString();
+        boolean correctText2 = Boolean.parseBoolean("victor stone");
 
-       trues = new boolean[]{correctText1,correctText2, ans1Correct,ans2Correct,ans3Correct,ans4Correct,ans5Correct,ans6Correct,ans7Correct};
+        RadioButton ans1 = (RadioButton) findViewById(R.id.q1c1);
+        boolean ans1Correct = ans1.isChecked();
 
-        for (boolean value : trues) {
-            if (!value)
-                return false;
+        CheckBox ans2 = (CheckBox) findViewById(R.id.q2c1);
+        boolean ans2Correct = ans2.isChecked();
+        CheckBox ans3 = (CheckBox) findViewById(R.id.q2c2);
+        boolean ans3Correct = ans3.isChecked();
+
+        RadioButton ans4 = (RadioButton) findViewById(R.id.q4c1);
+        boolean ans4Correct = ans4.isChecked();
+
+        CheckBox ans5 = (CheckBox) findViewById(R.id.q6c1);
+        boolean ans5Correct = ans5.isChecked();
+        CheckBox ans6 = (CheckBox) findViewById(R.id.q6c2);
+        boolean ans6Correct = ans6.isChecked();
+        CheckBox ans7 = (CheckBox) findViewById(R.id.q6c3);
+        boolean ans7Correct = ans7.isChecked();
+
+        answers.put("text", correctText1);
+        answers.put("text2", correctText2);
+        answers.put("q1c1", ans1Correct);
+        answers.put("ques2", question3(ans2Correct,ans3Correct));
+        answers.put("q4c1", ans4Correct);
+        answers.put("ques6", question6(ans5Correct,ans6Correct,ans7Correct));
+
+
+        int count = Collections.frequency(answers.values(), true);
+
+        TextView results = (TextView) findViewById(R.id.results);
+
+
+        switch(count){
+            case 0:
+                results.setText(getString(R.string.response1));
+                break;
+            case 1:
+                results.setText(getString(R.string.response2));
+                break;
+            case 2:
+                results.setText(getString(R.string.response3));
+                break;
+            case 3:
+               results.setText(getString(R.string.response4));
+               break;
+            case 4:
+                results.setText(getString(R.string.response5));
+                break;
+            case 5:
+               results.setText(getString(R.string.response6));
+                break;
+            case 6:
+               results.setText(getString(R.string.response7));
+               break;
+            default: results.setText("dfaskf");
+            break;
         }
-        return true;
+
     }
-
-
 }
 
